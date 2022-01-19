d3f2ff6e6e03290de4cd35e51ba632bd9e4a3c44
Shreyash Chede <66112931+Shreyash-Chede@users.noreply.github.com>
2022-01-19 10:38:19 +0530

    Create rscript.r

---
+++ rscript.r
@@ -0,0 +1,10 @@
+# R program to illustrate
+# printing output of an R program
+  
+# print string
+print("GFG")
+  
+# print variable
+# it will print 'GeeksforGeeks' on the console
+x <- "GeeksforGeeks"
+print(x) 
